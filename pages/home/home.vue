b9ce6b44b4f5d7446b931da279a696ecf2f753f0
lollipops-vc <898902463@qq.com>
2022-10-11 14:54:49 +0800

    search框

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,5 +1,9 @@
 <template>
-	<view>
+	<view class="home-container">
+		<view class="search-box" focus :class="{focus:isFocus}">
+				<input @focus="isFocus=true" @blur='isFocus=false' type="text" placeholder="搜索"/>
+				<icon type="search" size="20"></icon>
+		</view>
 		
 	</view>
 </template>
@@ -8,7 +12,7 @@
 	export default {
 		data() {
 			return {
-				
+				isFocus:false
 			}
 		},
 		methods: {
@@ -17,6 +21,32 @@
 	}
 </script>
 
-<style>
-
+<style lang="less">
+	.home-container{
+		.search-box{
+			background-color: #fe2c4b;
+			padding: 20rpx;
+			input{
+				background-color: white;
+				padding-left: 50%;
+				border-radius: 10rpx;
+				transition: .3s;
+			}
+			icon{
+				position: absolute;
+				top:25rpx;
+				left:43%;
+				transition: .3s;
+			}
+			//当search-box有focus的时候input和icon生效
+			&.focus{
+				input{
+					padding-left: 60rpx;
+				}
+				icon{
+					left:2%
+				}
+			}
+		}
+	}
 </style>
